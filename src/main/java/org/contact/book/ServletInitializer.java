2ae556e9b0c7eab62108ea37e55e9ad5e1b40f96
vlad-dvlad <vlad.ivashko72@gmail.com>
2021-03-25 13:22:10 +0200

    Spring Boot starter project

---
+++ src/main/java/org/contact/book/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.contact.book;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ContactBookApplication.class);
+	}
+
+}
